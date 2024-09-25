579b34fd21801a5c808a567c0ab7a77802e53238
joshiprashant09 <prasantnov9@gmail.com>
2024-09-26 00:09:23 +0530

    Create 2D array for chess board and respective color and square id

---
+++ data.js
@@ -0,0 +1,38 @@
+function square(color,id,piece){
+    return {
+        color,
+        id,
+        piece
+    }
+}
+function squareRow(rowId){
+    const squareRow=[];
+    const letters=['a','b','c','d','e','f','g','h'];
+
+    if(rowId%2===0){
+        letters.forEach((element,index) => {
+            if(index%2==0){
+                squareRow.push(square("white",element+rowId,null));
+            }
+            else{
+                squareRow.push(square("black",element+rowId,null));
+            }
+        });
+    }else{        
+        letters.forEach((element,index) => {
+            if(index%2==0){
+                squareRow.push(square("black",element+rowId,null));
+            }
+            else{
+                squareRow.push(square("white",element+rowId,null));
+            }
+        });
+    }
+    return squareRow;
+}
+
+function initGame(){
+     return [squareRow(8),squareRow(7),squareRow(6),squareRow(5),squareRow(4),squareRow(3),squareRow(2),squareRow(1)];
+}
+
+export {initGame};
