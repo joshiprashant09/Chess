ce2a63920fcbee4041a12d20f9fbeb147d20f3c7
joshiprashant09 <prasantnov9@gmail.com>
2024-09-26 00:51:23 +0530

    Import GameRendered

---
+++ script.js
@@ -0,0 +1,10 @@
+import { initGame } from "./data.js";
+import { initGameRender } from "./Render/main.js";
+
+// const element=document.createElement("div");
+// element.style.backgroundColor="#769656";
+// element.style.width="200px";
+// element.style.height="200px";
+
+// document.getElementsByTagName("div")[0].appendChild(element);
+initGameRender(initGame());
